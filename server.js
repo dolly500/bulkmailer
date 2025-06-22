e6217bc95d074404351f01709d383bbbcae1a7d2
“dolly500” <dolapoakamo01@gmail.com>
2025-06-22 03:10:28 +0100

    update

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use((req, res, next) => {
 app.use('/api', emailRoutes);
 
 // Health check
-app.get('/health', (req, res) => {
+app.get('/', (req, res) => {
   res.status(200).json({
     status: 'OK',
     service: 'Bulk Email API',
